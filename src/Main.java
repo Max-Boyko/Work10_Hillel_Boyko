d8cb8a7344b4d74ffaed70c5cb00df45f4ac5b08
Max Boyko <makcboukomail@gmail.com>
2022-10-02 20:24:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 9;
+
+        for(int i = 1; i <= 100; i ++){
+
+            if(i != a && i != b){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
